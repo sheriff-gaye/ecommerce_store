877674dc0fd9c28c4cc8ff78d873636c1f8f9ed5
sheriff-gaye <sheriffgaye5@gmail.com>
2023-10-13 00:22:08 +0000

    all

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    
+    <footer className='bg-white border-t'>
+        <div className='mx-auto  py-10'>
+            <p className='text-center text-xs text-black '>
+
+                &copy; 2023 Fakestore Name All right Resevered
+            </p>
+
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
